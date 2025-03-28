0e6533a5bea1caba0aa0302943a1443c35f65f81
Umer Saleem <umersalim42@gmail.com>
2025-03-28 23:09:58 +0500

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://courses_user:123@localhost:3306/courses"
+DATABASE_URL = "mysql+pymysql://database_user:password@localhost:port/database_name"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
